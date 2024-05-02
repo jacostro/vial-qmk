b68e06300534cde571fc49b27496ba374ab19d85
Jacek Ostrowski <jacostro@gmail.com>
2024-05-02 12:19:10 +0200

    Vial build with achordion and caps word

---
+++ keyboards/keychron/q11/ansi_encoder/keymaps/vial_jos/config.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x5C, 0x2B, 0xB4, 0x2A, 0x60, 0xB3, 0x8B, 0x46}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 9 }
+#define VIAL_UNLOCK_COMBO_COLS { 1, 7 }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 7
+#define VIAL_COMBO_ENTRIES 24
+#define VIAL_TAP_DANCE_ENTRIES 4
+
+#define PERMISSIVE_HOLD
+#define ACHORDION_STREAK
+#define CAPS_WORD_INVERT_ON_SHIFT
+#define CAPS_WORD_IDLE_TIMEOUT 5000
---
+++ keyboards/keychron/q11/ansi_encoder/keymaps/vial_jos/rules.mk
@@ -0,0 +1,17 @@
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+VIALRGB_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+KEY_OVERRIDE_ENABLE = no
+TAP_DANCE_ENABLE = no
+QMK_SETTINGS = yes
+CAPS_WORD_ENABLE = yes
+KEY_LOCK_ENABLE = yes
+TAP_DANCE_ENABLE = no
+LTO_ENABLE = yes
+
+SPLIT_KEYBOARD = yes
+
+VPATH += keyboards/keychron/common
+SRC += keychron_common.c
+SRC += features/achordion.c
